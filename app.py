fa4dcf237cc2ee40c3df6babbed83e1fab8e2084
LD <fld614@gmail.com>
2024-02-29 02:17:03 +0800

    update

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import openai
 from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader, TextLoader
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.embeddings import OpenAIEmbeddings
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.llms import OpenAI
@@ -91,7 +91,8 @@ def send_whatsapp_message(to_number, message):
     message1 = client.messages.create(
             from_='whatsapp:+14155238886',
             body=message,
-            to='whatsapp:+60187912826'
+            to=to_number
+            # to='whatsapp:+60187912826'
         )
     print(message1.sid)
 
