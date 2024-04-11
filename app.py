1c11d7b20c12c3241265247cd23b4b4f6f50afc2
LD <fld614@gmail.com>
2024-04-12 00:54:18 +0800

    update chat

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask, request, jsonify, session
 import openai
 from twilio.rest import Client
@@ -40,6 +41,7 @@ def handle_whatsapp_webhook():
         else:
             response = "Human mode is active. Please type /switch to switch to bot mode."
         send_whatsapp_message(from_number, response )
+        return "200 OK"
     
     print(session["chat_mode"])
     # only apply openai chat if the mode == BOT
@@ -47,16 +49,22 @@ def handle_whatsapp_webhook():
         # Initialize or get the message history from session state
         if "message_history" not in session:
             session["message_history"] = [
-                {"role": "assistant", "content": "Good day to you! Welcome to Lux Retreats! Thank you for your interest in our villas 🤩 We have two villas available, The Black Box Villa and The White Box Villa 🏘️ Both villas are conveniently located next to each other. Our clients have the option to rent them individually for small gatherings or book both villas for larger events. Please find the pricing details below"}
+                {"role": "assistant", "content": "Good day to you! Welcome to Lux Retreats! Thank you for your interest in our villas 🤩 We have two villas available, The Black Box Villa and The White Box Villa 🏘️ Both villas are conveniently located next to each other. Our clients have the option to rent them individually for small gatherings or book both villas for larger events. Please find the pricing details below"},
+                {"role": "system", "content": "calculate and anlayse the total cost in details"},
+                {"role": "system", "content": "contact Wendy at 016-3456126 to finalize the details"}
             ]
         message_history = session["message_history"]
+
+        message = re.sub(r'\xa0', ' ', message)  # Replace '\xa0' with space
+        message = message.replace('\n\n', '\n') 
+        message = re.sub(r'\s+', ' ', message).strip()
 
         # Add the user's message to the message history
         message_history.append({"role": "user", "content": message})
 
         response = openai.ChatCompletion.create(
             model = 'gpt-3.5-turbo-1106',
-            temperature = 1,
+            temperature= 0.3,  # Adjust temperature as needed
             messages = message_history
         )
         # Format the ChatGPT response
@@ -64,9 +72,11 @@ def handle_whatsapp_webhook():
 
         # Add the ChatGPT response to the message history
         message_history.append({"role": "assistant", "content": chat_response})
+        print('chat count' + str(len(message_history)))
         print(message_history)
+        session["message_history"] = message_history
         send_whatsapp_message(from_number, chat_response )
-
+        
     return "200 OK"
 
 def send_whatsapp_message(to_number, message):
